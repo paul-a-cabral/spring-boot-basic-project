c2d7fc23b8fcd70657f0b50a7b54aaf21aec5695
paul-a-cabral <paulcabral.acn@gmail.com>
2026-05-24 12:08:50 +0000

    removed unnecesary files
    Co-authored-by: Ona <no-reply@ona.com>

--- src/test/java/com/example/StudentControllerTest.java
+++ src/test/java/com/example/StudentControllerTest.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import static org.mockito.BDDMockito.given;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,7 +17,7 @@ import com.example.controller.StudentController;
 import com.example.entity.Student;
 import com.example.service.StudentService;
 
-//@WebMvcTest(StudentController.class)
+@WebMvcTest(StudentController.class)
 public class StudentControllerTest {
 
     @Autowired
